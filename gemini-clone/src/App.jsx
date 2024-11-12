b1df0e12f3807aeee2e8cf6ac8bf73fe0fd7b6ed
niharika <varshneyniharika2820@gmail.com>
2024-11-12 11:38:35 +0530

    final commit

---
+++ gemini-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sidebar from './components/sidebar/Sidebar'
+import Main from './components/main/Main'
+const App = () => {
+  return (
+    <>
+    <Sidebar></Sidebar>
+    <Main></Main>
+    </>
+  )
+}
+
+export default App
---
+++ gemini-clone/src/config/gemini.js
@@ -0,0 +1,38 @@
+
+
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  }  from "@google/generative-ai";
+  
+  const apiKey = "AIzaSyAXDWCcCY7g64NxPFvD5YDfstXi-FzWAAo";
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    const response =result.response;
+    console.log(response.text());
+  return response.text();
+  }
+  
+  export default run;
